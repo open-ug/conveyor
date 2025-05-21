a5fcfd710a56972c1eb80811d852fabd36993769
jim-junior <jimjunior854@gmail.com>
2025-05-21 16:48:30 +0300

    Package Clean up

--- cmd/cli/root.go
+++ cmd/cli/root.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2024 Cranom Technologies Limited, Beingana Jim Junior and Contributors
+Copyright © 2024 Beingana Jim Junior and Contributors
 */
 package cli
 
@@ -15,21 +15,17 @@ var Version string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "crane",
-	Short: "The Underlying Engine Behind Cranom Platform",
-	Long: `The Underlying Engine Behind Cranom Platform
-	
-	Learn More at: https://www.cranom.tech/plaform-tools/crane-engine
+	Use:   "conveyor",
+	Short: "Conveyor CLI",
+	Long: `Conveyor CLI is a command line interface for managing and interacting with the Conveyor platform.
 	`,
 	Version: Version,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Welcome to Crane Orchastrator Engine")
-		fmt.Println("Use cranom --help to see available commands")
+		fmt.Println("Welcome to Conveyor CLI")
+		fmt.Println("Use conveyor --help to see available commands")
 	},
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -41,17 +37,6 @@ func init() {
 	cobra.OnInitialize(config.InitConfig)
 
 	rootCmd.AddCommand(APIServerCmd)
-	rootCmd.AddCommand(DockerDriverCmd)
-	rootCmd.AddCommand(NginxDriverCMD)
-	rootCmd.AddCommand(CertBotDriverCmd)
-	rootCmd.AddCommand(WebhookDriverCmd)
-	rootCmd.AddCommand(BuildPacksDriverCmd)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	rootCmd.PersistentFlags().StringVar(&config.CfgFile, "config", "", "config file (default is /etc/crane/config.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
