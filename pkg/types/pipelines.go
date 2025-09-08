744182522ba3523db031a95a29d95c5b3916fa87
jim-junior <jimjunior854@gmail.com>
2025-09-08 22:19:04 +0300

    feat: implement pipeline management with CRUD operations and routes

---
+++ pkg/types/pipelines.go
@@ -0,0 +1,16 @@
+package types
+
+type Pipeline struct {
+	Name        string            `json:"name"`
+	Description string            `json:"description"`
+	Version     string            `json:"version"`
+	Resource    string            `json:"resource"`
+	Steps       []Step            `json:"steps"`
+	Metadata    map[string]string `json:"metadata"`
+}
+
+type Step struct {
+	ID     string `json:"id"`
+	Name   string `json:"name"`
+	Driver string `json:"driver"`
+}
