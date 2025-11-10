09660676b130366c3cdfcbb45fbebf2080d9eb92
jim-junior <jimjunior854@gmail.com>
2025-11-10 08:56:02 +0300

    fix: correct tag formatting for Ubuntu images in Docker build workflow

--- cmd/api/app.go
+++ cmd/api/app.go
@@ -3,7 +3,7 @@ Copyright © 2024 - Present Conveyor CI Contributors
 */
 
 // @title Conveyor CI API
-// @version 0.3.0
+// @version 0.3.2
 // @description Conveyor is a lightweight, distributed CI/CD engine built for platform developers who demand simplicity without compromise.
 
 // @contact.name Conveyor Support
